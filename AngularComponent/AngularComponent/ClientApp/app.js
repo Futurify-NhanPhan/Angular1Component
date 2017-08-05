395625668240634febbedf9663cac62a4e4df35d
Nhan Phan <nhanphan@futurify.vn>
2017-08-05 14:37:42 +0700

    Init angular component

---
+++ AngularComponent/AngularComponent/ClientApp/app.js
@@ -0,0 +1,22 @@
+﻿(function (angular) {
+    'use strict';
+    angular.module('app', ['ngComponentRouter', 'test'])
+
+    .config(function ($locationProvider) {
+        $locationProvider.html5Mode(true);
+    })
+
+    .value('$routerRootComponent', 'app')
+
+    .component('app', {
+        template:
+          '<nav>\n' +
+          '  <a ng-link="[\'Test\']">Go to test page</a>\n' +
+          '</nav>\n' +
+          '<ng-outlet></ng-outlet>\n',
+        $routeConfig: [
+          { path: '/test', name: 'Test', component: 'testComponent', useAsDefault: true },
+      
+        ]
+    });
+})(window.angular);
---
+++ AngularComponent/AngularComponent/ClientApp/test-component.js
@@ -0,0 +1,20 @@
+﻿(function (angular) {
+    'use strict';
+    angular.module('test', []) 
+        .component('testComponent', {
+            template:
+              '<h2>{{$ctrl.textMessage}}</h2>',
+            bindings: { $router: '<' },
+            controller: ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
+                var $ctrl = this;
+                $ctrl.textMessage = 'Hello everyone!';               
+            }]
+        });   
+    
+})(window.angular);
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
